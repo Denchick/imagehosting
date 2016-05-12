b8397afba5ac6f4e15e748f665632f03d1aaf6bf
Denchick <denchick1997@mail.ru>
2016-05-12 11:42:58 +0500

    My first commit

---
+++ upload/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Image(models.Model):
+	slug = models.SlugField(max_length=50)
+	title = models.CharField(max_length=200)
+	upload = models.ImageField(upload_to='media/uploads')
+	created_date = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return "/news/%s" % (self.news_date.strftime('%Y%m%d') + '-' + str(self.id))
